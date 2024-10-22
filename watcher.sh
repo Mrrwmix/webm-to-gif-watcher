303ad819c597ea39e934df0123d9085813604e19
Mrrwmix <32693786+Mrrwmix@users.noreply.github.com>
2024-10-22 17:08:29 -0500

    logging updated

--- watcher.sh
+++ watcher.sh
@@ -2,15 +2,16 @@
 CODE_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export $(grep -v '^#' $CODE_DIRECTORY/.env | xargs)
 
-echo $WEBM_DIRECTORY
-echo $GIF_DIRECTORY
+echo incoming .webm directory: $WEBM_DIRECTORY
+echo output .gif directory: $GIF_DIRECTORY
 
 inotifywait -m $WEBM_DIRECTORY -e create -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
         newWebmFile="$path$file"
         gifOutputFile="$GIF_DIRECTORY${file%.*}.gif"
-        echo "$newWebmFile"
-        echo "$gifOutputFile"
+        
         ffmpeg -i "$newWebmFile" -pix_fmt rgb24 "$gifOutputFile"
+
+        echo "$gifOutputFile created"
     done
